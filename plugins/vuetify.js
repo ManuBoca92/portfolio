3fba52964eebb334ab1df6fd1a2675f2e6c5596d
Ebonom Mfam <ebonom.mfam@snow.dog>
2019-11-13 20:50:46 +0100

    added material design font package

--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify/lib'
 import colors from 'vuetify/es5/util/colors'
+import '@mdi/font/css/materialdesignicons.css'
 
 Vue.use(Vuetify, {
   theme: {
@@ -10,6 +11,7 @@ Vue.use(Vuetify, {
     info: colors.teal.lighten1,
     warning: colors.amber.base,
     error: colors.deepOrange.accent4,
-    success: colors.green.accent3
+    success: colors.green.accent3,
+    iconfont: 'mdi'
   }
 })
