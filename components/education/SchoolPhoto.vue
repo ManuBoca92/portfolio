7f96681dde477079f117e269ecd1d1ed9d4c2418
Ebonom Mfam <ebonom.mfam@snow.dog>
2019-11-13 23:23:50 +0100

    added urls to photos

--- components/education/SchoolPhoto.vue
+++ components/education/SchoolPhoto.vue
@@ -1,13 +1,15 @@
 <template>
   <v-flex xs3 sm3 md3 lg3 xl3>
     <v-list-item-avatar>
-      <v-img :src=avatar></v-img>
+      <a :href="url" target="_blank">
+        <v-img :src="avatar" />
+      </a>
     </v-list-item-avatar>
   </v-flex>
 </template>
 
 <script>
 export default {
-  props: ['avatar']
+  props: ['avatar', 'url']
 }
 </script>
